399ca2dc428b017fa741f1075fc0631a73a81304
MBaran88 <zdzibq@gmail.com>
2017-05-23 18:26:04 +0200

    Fourth commit

--- src/com/QuicksortTrue/Engine.java
+++ src/com/QuicksortTrue/Engine.java
@@ -21,11 +21,38 @@ public class Engine {
             while (numbers[i] < pivot){
                 i++;
             }
-
+            // If the current value from the right list is larger than the pivot
+            // element then get the next element from the right list
+            while (numbers[j] > pivot){
+                i--;
+            }
+            // If we have found a value in the left list which is larger than
+            // the pivot element and if we have found a value in the right list
+            // which is smaller than the pivot element then we exchange the
+            // values.
+            // As we are done we can increase i and j
+            if (i <= j) {
+                exchange(i, j);
+                i++;
+                j--;
+            }
         }
+        // Recursion
+        if (low < j)
+            quicksort(low, j);
+        if (i < high)
+            quicksort(i, high);
+    }
 
+    private void exchange(int i, int j) {
+        //swapping two numbers
+        int temp = numbers[i];
+        numbers[i] = numbers [j];
+        numbers[j] = temp;
     }
 
 }
+
+
 
 
